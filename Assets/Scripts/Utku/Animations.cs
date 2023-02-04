d65f724ccce3ae31382edb8c125cfaa9c0327ce9
kayisutku <kayisutku@gmail.com>
2023-02-04 20:04:15 +0300

    animator

--- Assets/Scripts/Utku/Animations.cs
+++ Assets/Scripts/Utku/Animations.cs
@@ -22,22 +22,13 @@ public class Animations : MonoBehaviour
     {
         isAttack = false;
         anim.SetLayerWeight(animLayerIndex, 0);
-        //anim.SetTrigger("TriggerUP");
-        
-        //anim.Play("Air Flip",0,0);
-        
         anim.CrossFade("Air Flip",0.1f,0,0.07f);
     }
 
     public void PunchAnim()
     {
         isAttack = true;
-        anim.SetLayerWeight(animLayerIndex, 1);
-        //anim.SetTrigger("TriggerPunch");
-        //anim.Play("Punch",0,0);
-        
-        //anim.Play("Sword Swing",1,0.37f);
-        
+        anim.SetLayerWeight(animLayerIndex, 1);       
         anim.CrossFade("Sword Swing",0.1f,1,0.37f);
     }
 
