d4045e08b1bf3e1e5826c097ce1e80973911c527
malperbaysal <30733455+malperbaysal@users.noreply.github.com>
2023-02-06 09:22:17 +0300

    :P

--- Assets/Scripts/Alper/YenidenPrefabimsi.cs
+++ Assets/Scripts/Alper/YenidenPrefabimsi.cs
@@ -6,25 +6,42 @@ using UnityEngine;
 public class YenidenPrefabimsi : MonoBehaviour
 {
     public List<GameObject> squares;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> news;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public enum Level {first, second, third, forth};
 
+    public Level level;
+    public GameObject fi, se, th, fo;
+    public bool once;
     private void OnValidate()
     {
+        if(once)
+            return;
+        once=true;
         squares.Clear();
         foreach (var rigidbody in GetComponentsInChildren<Rigidbody>())
         {
             if(rigidbody.transform.parent.gameObject.activeSelf)
                 squares.Add(rigidbody.transform.parent.gameObject);
+        }
+
+        
+        PlaceEnemies();
+    }
+
+    void PlaceEnemies()
+    {
+        GameObject toInstantia=fi;
+        
+
+        foreach (var VARIABLE in squares)
+        {
+            var fgjnb= Instantiate(fi, VARIABLE.transform.position, fi.transform.rotation);
+            fgjnb.transform.parent = VARIABLE.transform.GetChild(0);
+            fgjnb.transform.parent.localScale = Vector3.one;
+            fgjnb.name = "sikik";
+            VARIABLE.GetComponentInParent<MeshRenderer>().enabled = false;
+            news.Add(fgjnb);
         }
     }
 }
